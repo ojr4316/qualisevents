4ed4fbc3f9ead977ebe58b402dee8421bf02d47e
JosephsenC <cjosephsen123@gmail.com>
2022-08-02 16:30:01 -0400

    portfolio page + contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,8 +6,20 @@ import buttonStyles from "../styles/ButtonStyles.module.css";
 import InfoImage from "../components/InfoImage";
 import Layout from "../components/Layout";
 import welcome from "../public/welcome.png";
+import { useState } from "react";
 
 export default function Contact() {
+
+  const[firstName, setFirstName] = useState("");
+  const[lastName, setLastName] = useState("");
+  const[phone, setPhone] = useState("");
+  const[email, setEmail] = useState("");
+  const[inquiry, setInquiry] = useState("");
+
+  function onSubmit() {
+    console.log(firstName, lastName, phone, email, inquiry);
+  }
+
   return (
     <Layout page="contact">
       <div className={styles.contact}>
@@ -25,33 +37,34 @@ export default function Contact() {
             style={{ marginRight: "32px" }}
           >
             <label className={styles.form_label}>First Name</label>
-            <input type="text" className={styles.form_input} />
+            <input type="text" className={styles.form_input} value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
           </div>
           <div className={styles.form_container}>
             <label className={styles.form_label}>Last Name</label>
-            <input type="text" className={styles.form_input} />
+            <input type="text" className={styles.form_input} value={lastName} onChange={(e) => setLastName(e.target.value)}/>
           </div>
           <div
             className={styles.form_container}
             style={{ marginRight: "32px" }}
           >
             <label className={styles.form_label}>Phone</label>
-            <input type="text" className={styles.form_input} />
+            <input type="text" className={styles.form_input} value={phone} onChange={(e) => setPhone(e.target.value)}/>
           </div>
           <div className={styles.form_container}>
             <label className={styles.form_label}>Email</label>
-            <input type="text" className={styles.form_input} />
+            <input type="text" className={styles.form_input} value={email} onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div
             className={styles.form_container}
             style={{ gridColumn: "1 / 3 " }}
           >
             <label className={styles.form_label}>Inquiry</label>
-            <textarea className={styles.form_textarea}></textarea>
+            <textarea className={styles.form_textarea} value={inquiry} onChange={(e)=>setInquiry(e.target.value)}></textarea>
           </div>
           <button
             className={buttonStyles.normal}
             style={{ marginTop: "24px", width: "220px" }}
+            onClick={onSubmit}
           >
             <p className={buttonStyles.normal_text}>Submit</p>
           </button>
